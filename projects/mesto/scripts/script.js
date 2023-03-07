4ee08d47d0838ff630f19464c66af5b854b66722
Stas Zaitsev <ststas@yandex.ru>
2023-03-08 00:24:22 +0200

    mesto updated

--- projects/mesto/scripts/script.js
+++ projects/mesto/scripts/script.js
@@ -11,13 +11,12 @@ let profileOccupation = document.querySelector('.profile__occupation');
 let formElement = document.querySelector('.popup__form')
 let popupName = document.querySelectorAll('.popup__field')[0];
 let popupOccupation = document.querySelectorAll('.popup__field')[1];
-// передаем значение имени и профессии из профиля в поля редактирования всплывающего окна
-popupName.value = profileName.textContent;
-popupOccupation.value = profileOccupation.textContent;
 
 //функции открытия и закрытия всплывающего окна 
 function popupOpen() {
   popup.classList.add('popup_opened');
+  popupName.value = profileName.textContent;
+  popupOccupation.value = profileOccupation.textContent;
 }
 function popupClose() {
   popup.classList.remove('popup_opened');
@@ -28,10 +27,10 @@ function submitForm(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileOccupation.textContent = popupOccupation.value;
+  popupClose();
 }
 
 // назначаем обработчики событий
 editButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
 formElement.addEventListener('submit', submitForm);
-submitButton.addEventListener('click', popupClose);
