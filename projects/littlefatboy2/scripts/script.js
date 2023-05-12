f800efebbcaef29506d60316a4b6aa31e1ad312f
Stas Zaitsev <ststas@yandex.ru>
2023-05-12 20:22:48 +0300

    update little fat boy

--- projects/littlefatboy2/scripts/script.js
+++
@@ -1,9 +0,0 @@
-const headerMenuButtonElement = document.querySelector('.header__menu-button-container');
-const headerMenuElement = document.querySelector('.header__menu');
-console.log(headerMenuButtonElement);
-
-const openHeaderMenu = function() {
-  headerMenuElement.classList.toggle('header__menu_opened')
-}
-
-headerMenuButtonElement.addEventListener('click', openHeaderMenu)
